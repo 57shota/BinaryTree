f1992fcb9b0d3d6005c081048f7662424a1dbb1e
57shota <sho.weblife@gmail.com>
2019-04-04 10:58:20 +1100

    initial ver of the project

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,103 @@
+﻿using System;
+namespace BinaryTree{
+
+    // Node of tree
+    class MyBinaryTreeNode{
+        
+        public int Data;
+        public MyBinaryTreeNode Left;
+        public MyBinaryTreeNode Right;
+
+        public MyBinaryTreeNode(){
+            
+            this.Data = 0;
+            this.Left = null;
+            this.Right = null;
+
+        }
+
+        public MyBinaryTreeNode(int value){
+            
+            this.Data = value;
+            this.Left = null;
+            this.Right = null;
+
+        }
+    }
+
+    class BinaryTree {
+
+        public enum TraversalOrder{
+            
+            PreOrder,
+            InOrder,
+            PostOrder
+
+        }
+
+        public MyBinaryTreeNode Root;
+
+        public BinaryTree(){
+            
+            this.Root = null;
+
+        }
+
+        public void Dump()
+        {
+            this.PrintTree(this.Root, 0);
+            return;
+        }
+
+        private void PrintTree(MyBinaryTreeNode node, int role, string indent = "")
+        {
+            string prefix = "";
+            switch (role)
+            {
+                case 0: // root node
+                    prefix = "──  Root ";
+                    break;
+                case -1: // left node
+                    prefix = indent + "├──  left  ";
+                    break;
+                case 1: // right node
+                    prefix = indent + "└──  right ";
+                    break;
+                default:
+                    break;
+            }
+
+            if (role != 0)
+            {
+                Console.WriteLine($"{indent}│");
+            }
+
+            if (node == null)
+            {
+                Console.WriteLine($"{prefix} null");
+            }
+            else
+            {
+                Console.WriteLine($"{prefix} {node.Data}");
+                string padding = (role == -1) ? "│    " : "     ";
+
+                this.PrintTree(node.Left, -1, indent + padding);
+                this.PrintTree(node.Right, 1, indent + padding);
+            }
+
+            //log.debug(prefix + nodeConnection + nodeName);
+            return;
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BinaryTree{
+    
+    class MainClass{
+        
+        public static void Main(string[] args){
+            
+            Console.WriteLine("--- Assignment2 / Binary tree ---");
+
+            BinaryTree tree = new BinaryTree();
+
+            tree.Root = new MyBinaryTreeNode(2);
+            tree.Root.Left = new MyBinaryTreeNode(7);
+            tree.Root.Left.Left = new MyBinaryTreeNode(2);
+            tree.Root.Left.Right = new MyBinaryTreeNode(6);
+            tree.Root.Left.Right.Left = new MyBinaryTreeNode(5);
+            tree.Root.Left.Right.Right = new MyBinaryTreeNode(11);
+            tree.Root.Right = new MyBinaryTreeNode(5);
+            tree.Root.Right.Right = new MyBinaryTreeNode(9);
+            tree.Root.Right.Right.Left = new MyBinaryTreeNode(4);
+            tree.Dump();
+
+        }
+    }
+}
